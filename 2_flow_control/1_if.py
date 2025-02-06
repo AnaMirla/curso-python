2cca50445f4302adbc08988e98f42e7df70c9ec9
mbelpines <anamirlacuevas@gmail.com>
2025-02-05 22:41:12 -0400

    Clase 2 (sin terminar)

---
+++ 2_flow_control/1_if.py
@@ -0,0 +1,85 @@
+#Sentencias condicionaes (If, elif, else)
+
+# import os #Importamos la librería os para poder limpiar la pantalla
+# os.system("clear")
+
+# print("Sentencia simple condicional")
+
+# edad=18
+
+# if edad >= 18:
+#     print("Eres mayor de edad")
+
+# print ("Sentencia condicional con else")
+
+# edad= int(input("Ingresa tu edad: "))
+
+# if edad > 18:
+#     print("Felicidades! Eres un viejo verde 👵🏻👴🏻")
+# else:
+#     print("Eres un chicuelo👶🏻🧒🏻")
+
+# print("-------------------")
+
+# print("Sentencia condicional con elif")
+
+# nota= int(input("Ingresa tu nota: "))
+
+# if nota >= 9:
+#     print("Felicidades! Tienes un 10")
+# elif nota >= 7:
+#     print("Notable")
+# elif nota >= 5:
+#     print("Aprobaste")
+# else:
+#     print("Reprobaste")
+
+# print("-------------------")
+
+# print ("Condiciones múltiples con and")
+
+# edad= int(input("Ingresa tu edad: "))
+# carnet= True
+
+# if edad >= 18 and carnet:
+#     print("Puedes conducir 🚗")
+# else:
+#     print("POLICIAAA 🚔🚔")
+
+# print("-------------------")
+
+# print ("Condiciones múltiples con or")
+
+# edad= int(input("Ingresa tu edad: "))
+# carnet= True
+# if edad >= 18 or carnet:
+#     print("Puedes conducir 🚗")
+# else:
+#     print("POLICIAAA 🚔🚔")
+
+print("-------------------")
+es_fin_de_semana= False
+if not es_fin_de_semana:
+    print("A trabajar Anita! 💼")
+
+print("-------------------")
+
+# print("Condiciones anidadas")
+
+# edad= 18
+# tener_dinero: True
+
+# if edad >= 18:
+#     if tener_dinero:
+#         print("Puedes ir a la disco 🕺🏻💃🏻")
+#     else:
+#         print("Quédate en tu casa wey 🏠")
+# else:
+#     print("No puedes entrar a la disco 🚫")
+
+# if edad < 18:
+#     print("No puedes entrar a la disco 🚫")
+# elif tener_dinero:
+#     print("Puedes ir a la disco 🕺🏻💃🏻")
+# else:
+#     print("Quédate en tu casa wey 🏠")
---
+++ 2_flow_control/3_list.py
@@ -0,0 +1,28 @@
+#Listas
+
+print("Crear listas")
+
+lista1= [1, 2, 3, 4, 5]
+lista2= ["ana", "betsaida", "cecilia", "diana", "esther"]
+lista3= [1, "hola", 3.14, True, False]
+
+lista_vacia= []
+lista_de_listas= [[1,2], [3,4], [5,6]]
+
+# print(lista1)
+# print(lista2)
+# print(lista3)
+# print(lista_vacia)
+# print(lista_de_listas)
+
+#Acceso a elementos por indice
+
+# print("\nAcceso a elementos por indice")
+# print()
+# print(lista1[0]) 
+# print(lista2[1])
+# print(lista2[-1]) #muestra el ultimo elemento
+
+#Slicing (rebanado) de listas
+print(lista1[1:3]) #muestra los elementos de la posicion 1 a la 3
+
